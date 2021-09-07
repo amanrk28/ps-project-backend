54abcf60eb82205d385d0d8e15c212caf6633b9f
AmanRK <amankhemka.ak28@gmail.com>
2021-09-08 01:59:24 +0530

    Changed cart_items in cart to have product ids only

--- ps-project-backend/store/serializers.py
+++ ps-project-backend/store/serializers.py
@@ -15,21 +15,8 @@ class CartSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
         exclude = ('deleted', 'created_on', 'updated_on')
 
     def get_cart_items(self, instance):
-        cart_items_queryset = CartItem.objects.filter(cart=instance).values_list('product_id','product__image', 'product__name', 'product__price', 'quantity')
-        cart_items = list()
-        for item in list(cart_items_queryset):
-            data = {'product_id': item[0], 'image': item[1], 'name': item[2], 'price': item[3], 'quantity': item[4]}
-            data.update({'amount': data['price'] * data['quantity']})
-            cart_items.append(data)
-        return cart_items
+        return list(CartItem.objects.filter(cart=instance).values_list('product_id', flat=True))
 
-    def to_representation(self, instance):
-        data = super(CartSerializer, self).to_representation(instance)
-        amt = 0.0
-        for item in data['cart_items']:
-            amt += item['amount']
-        data['total_amount'] = amt
-        return data
 
 class CartItemSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     cart_count = serializers.SerializerMethodField(read_only=True)
--- ps-project-backend/store/urls.py
+++ ps-project-backend/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('product', views.ProductList.as_view()),
     path('product/<int:pk>', views.ProductDetail.as_view()),
-    path('product/categories', views.get_product_categories),
+    path('product_categories', views.get_product_categories),
     path('cart', views.CartItemList.as_view()),
     path('cart/<int:pk>', views.CartItemDetail.as_view()),
     path('checkout', views.checkout_page_details)
